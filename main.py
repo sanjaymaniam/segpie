b6dbff9eed63dde162c04d31d0a5d0cfea6d9f15
cyphysan <sanjay.cyphy@gmail.com>
2018-12-18 11:48:42 +0530

    model restructure

--- main.py
+++ main.py
@@ -2,10 +2,13 @@ import tensorflow as tf
 import numpy as np
 from PIL import Image
 import matplotlib.pyplot as plt
-import model as net
+import model_t as net
 import os
 
-BATCH_SIZE = 1
+batch_size = 1
+epochs = 500
+summaries_dir = './summaries'
+
 image = (np.array(Image.open("./CamVid/train/0001TP_006690.png"))).astype('float32').reshape(1,360,480,3)
 label = (np.array(Image.open("./CamVid/trainannot/0001TP_006690.png"))).astype('int').reshape(1,360,480)
 
@@ -14,30 +17,30 @@ label = (np.array(Image.open("./CamVid/trainannot/0001TP_006690.png"))).astype('
 # # plt.imshow(label)
 # plt.show()label
 
-image_input = tf.placeholder(tf.float32, shape=(BATCH_SIZE, 360, 480, 3), name="input")
-label_input = tf.placeholder(tf.int32, shape=(BATCH_SIZE, 360, 480), name="label")
+image_input = tf.placeholder(tf.float32, shape=(batch_size, 360, 480, 3), name="input")
+label_input = tf.placeholder(tf.int32, shape=(batch_size, 360, 480), name="label")
 # is_training =tf.placeholder(tf.bool, "is_training")
+is_training = tf.placeholder_with_default(False, shape=(),name='is_training')
 
-with tf.name_scope("feed_forward"):
-    preds = net.predict(image_input)
-with tf.name_scope("loss"):
-    loss = net.loss(preds, label_input)
-with tf.name_scope("train"):
-    train = tf.train.AdamOptimizer(1e-4).minimize(loss)
+preds = net.predict(image_input, is_training)
 
-"""
+
+loss = net.loss(preds, label_input)
+update_ops = tf.get_collection(tf.GraphKeys.UPDATE_OPS)
+with tf.control_dependencies(update_ops):
+        train = tf.train.AdamOptimizer().minimize(loss)
+
 merged = tf.summary.merge_all()
-if not os.path.exists('summaries'):
-    os.mkdir('summaries')
-if not os.path.exists(os.path.join('summaries','first')):
-    os.mkdir(os.path.join('summaries','first'))
-"""
 
 sess = tf.Session()
+train_writer = tf.summary.FileWriter(summaries_dir + '/train', sess.graph)
+test_writer = tf.summary.FileWriter(summaries_dir + '/test')
 # summ_writer = tf.summary.FileWriter(os.path.join('summaries','first'), sess.graph)
 sess.run(tf.global_variables_initializer())
-for step in range(500):
-    _, _loss = sess.run([train, loss], feed_dict={image_input: image, label_input: label})
+
+for step in range(epochs):
+    _, _loss, summary = sess.run([train, loss, merged], feed_dict={image_input: image, label_input: label, is_training: True})
     if step % 10 == 0:
+        train_writer.add_summary(summary, step)
         print("step= ",step)
         print("loss= ",_loss)
--- model.py
+++ model.py
@@ -2,39 +2,37 @@ import tensorflow as tf
 import numpy as np
 
 def variable_summaries(var):
-    """Attach a lot of summaries to a Tensor (for TensorBoard visualization)."""
-    with tf.name_scope('summaries'):
-      mean = tf.reduce_mean(var)
-      tf.summary.scalar('mean', mean)
-      with tf.name_scope('stddev'):
-        stddev = tf.sqrt(tf.reduce_mean(tf.square(var - mean)))
-      tf.summary.scalar('stddev', stddev)
-      tf.summary.scalar('max', tf.reduce_max(var))
-      tf.summary.scalar('min', tf.reduce_min(var))
-      tf.summary.histogram('histogram', var)
+  with tf.name_scope('summaries'):
+    mean = tf.reduce_mean(var)
+    tf.summary.scalar('mean', mean)
+    with tf.name_scope('stddev'):
+      stddev = tf.sqrt(tf.reduce_mean(tf.square(var - mean)))
+    tf.summary.scalar('stddev', stddev)
+    tf.summary.scalar('max', tf.reduce_max(var))
+    tf.summary.scalar('min', tf.reduce_min(var))
+    tf.summary.histogram('histogram', var)
 
-def conv(x, D, K, F, S, scope):
+def conv(x, training, D, K, F, S, scope):
     with tf.variable_scope(scope):
         strides = [1, S, S, 1]
-        with tf.name_scope('filter_weights'):
+        with tf.variable_scope('filter'):
             filter = tf.get_variable('filter', [F,F,D,K],
-             initializer = tf.truncated_normal_initializer(stddev=0.1))
+             initializer = tf.truncated_normal_initializer(stddev=0.01))
             variable_summaries(filter)
-        with tf.name_scope('bias'):
-            bias = tf.get_variable('bias', [K], initializer = tf.constant_initializer(0.01))
-            variable_summaries(bias)
-        with tf.name_scope('convolved_output'):
-            x = tf.nn.conv2d(x, filter, strides, padding = 'SAME', name=scope)
-            tf.summary.histogram('pre-activations', x)
-            x = tf.nn.relu(tf.nn.bias_add(x,bias))
-            tf.summary.histogram('activations', x)
+        with tf.variable_scope('bias'):
+            bias = tf.get_variable('bias', [K], initializer = tf.constant_initializer(0))
+            # variable_summaries(bias)
+        x = tf.nn.bias_add(tf.nn.conv2d(x, filter, strides, padding = 'SAME', name=scope), bias)
+        # x = tf.layers.batch_normalization(x, training=is_train)
+        x = tf.nn.relu(x)
+        tf.summary.histogram(scope, x)
         return x
 
 def max_pool(x, F, S, scope):
-    with tf.name_scope(scope):
+    with tf.variable_scope(scope):
         strides = [1,S,S,1]
         op,indices = tf.nn.max_pool_with_argmax(x, [1, F, F, 1], strides, padding = 'SAME')
-        tf.summary.histogram('max_pool_outputs', op)
+        tf.summary.histogram(scope, op)
         return op, indices
 
 """
@@ -67,59 +65,55 @@ def max_indices_unpool(pool, ind, S=2 , scope='unpool_2d'):
 
 def unpool(x, output_shape,D,K,F, S, scope): #transpose convolution
     with tf.variable_scope(scope):
-        with tf.name_scope('filter'):
+        with tf.variable_scope('weights'):
             filter = tf.get_variable('filter', [F,F,D,K],
-             initializer = tf.truncated_normal_initializer(stddev=0.1))
+             initializer = tf.truncated_normal_initializer(stddev=0.01))
             variable_summaries(filter)
         strides = [1,S,S,1]
-        with tf.name_scope('unpool_outputs'):
-            op = tf.nn.conv2d_transpose(x, filter, output_shape,strides,padding = 'SAME')
-            tf.summary.histogram('unpool_outputs', op)
-    return op
+        op = tf.nn.conv2d_transpose(x, filter, output_shape,strides,padding = 'SAME')
+        tf.summary.histogram(scope, op)
+        return op
 
 def loss(input_preds, input_labels):
-    with tf.name_scope('one_hot_reshape'):
-        one_hot_labels = tf.one_hot(input_labels, depth=12)
-    with tf.name_scope('cross_entropy'):
+    one_hot_labels = tf.one_hot(input_labels, depth=12)
+    with tf.variable_scope("cross_entropy"):
         ce_loss = tf.nn.softmax_cross_entropy_with_logits_v2(labels=one_hot_labels,logits=input_preds, name='cross_entropy')
-    # ce_loss = tf.softmax_cross_entropy_with_logits(labels=input_labels,logits=input_preds, name='cross_entropy')
+        # ce_loss = tf.softmax_cross_entropy_with_logits(labels=input_labels,logits=input_preds, name='cross_entropy')
         ce_mean = tf.reduce_mean(ce_loss, name='ce_mean')
         tf.summary.scalar('cross_entropy', ce_mean)
-    return ce_mean
+        return ce_mean
 
-def predict(image):
-    img = tf.nn.lrn(image, depth_radius=5, bias=1.0, alpha=0.0001, beta=0.75, name='norm_img')
+def predict(image, training):
+    img = tf.nn.lrn(image, depth_radius=5, bias=0.01, alpha=0.0001, beta=0.75, name='norm_img')
 
-    with tf.variable_scope('Encoders'):
-        conv1 = conv(img, D=3, K=64, F=7, S=1, scope = "conv1")
-        pool1, pool1_indices = max_pool(conv1, F=2, S=2, scope = "pool1") #180*240
+    conv1 = conv(img, training, D=3, K=64, F=3, S=1, scope = "conv1")
+    pool1, pool1_indices = max_pool(conv1, F=2, S=2, scope = "pool1") #180*240
 
-        conv2 = conv(pool1, D=64, K=64, F=7, S=1, scope = "conv2")
-        pool2, pool2_indices = max_pool(conv2, F=2, S=2, scope = "pool2") #90*120
+    conv2 = conv(pool1, training, D=64, K=64, F=3, S=1, scope = "conv2")
+    pool2, pool2_indices = max_pool(conv2, F=2, S=2, scope = "pool2") #90*120
 
-        conv3 = conv(pool2, D=64, K=64, F=7, S=1, scope = "conv3")
-        pool3, pool3_indices = max_pool(conv3, F=2, S=2, scope = "pool3") #45*60
+    conv3 = conv(pool2, training, D=64, K=64, F=3, S=1, scope = "conv3")
+    pool3, pool3_indices = max_pool(conv3, F=2, S=2, scope = "pool3") #45*60
 
-        conv4 = conv(pool3, D=64, K=64, F=7, S=1, scope = "conv4")
-        pool4, pool4_indices = max_pool(conv4, F=2, S=2, scope = "pool4") #23*30
+    conv4 = conv(pool3, training, D=64, K=64, F=3, S=1, scope = "conv4")
+    pool4, pool4_indices = max_pool(conv4, F=2, S=2, scope = "pool4") #23*30
 
-    with tf.variable_scope('Decoders'):
-        unpool4 = unpool(pool4, conv4.get_shape(),64,64,2, S=2, scope="unpool4")
-        # unpool4 = max_indices_unpool(pool4, pool4_indices, S=2, scope = "unpool4")
-        deconv4 = conv(unpool4, D = 64, K=64, F=7, S=1, scope = "deconv4")
+    unpool4 = unpool(pool4, conv4.get_shape(),64,64,2, S=2, scope="unpool4")
+    # unpool4 = max_indices_unpool(pool4, pool4_indices, S=2, scope = "unpool4")
+    deconv4 = conv(unpool4, training, D = 64, K=64, F=3, S=1, scope = "deconv4")
 
-        unpool3 = unpool(pool3, conv3.get_shape(),64,64,2, S=2, scope="unpool3")
-        # unpool3 = max_indices_unpool(deconv4, pool3_indices, S=2, scope = "unpool3")
-        deconv3 = conv(unpool3, D=64, K=64, F=7, S=1, scope = "deconv3")
+    unpool3 = unpool(deconv4, conv3.get_shape(),64,64,2, S=2, scope="unpool3")
+    # unpool3 = max_indices_unpool(deconv4, pool3_indices, S=2, scope = "unpool3")
+    deconv3 = conv(unpool3, training, D=64, K=64, F=3, S=1, scope = "deconv3")
 
-        unpool2 = unpool(pool2, conv2.get_shape(),64,64,2, S=2, scope="unpool2")
-        # unpool2 = max_indices_unpool(deconv3, pool2_indices, S=2, scope = "unpool2")
-        deconv2 = conv(unpool2, D=64, K=64, F=7, S=1, scope = "deconv2")
+    unpool2 = unpool(deconv3, conv2.get_shape(),64,64,2, S=2, scope="unpool2")
+    # unpool2 = max_indices_unpool(deconv3, pool2_indices, S=2, scope = "unpool2")
+    deconv2 = conv(unpool2, training,D=64, K=64, F=3, S=1, scope = "deconv2")
 
-        unpool1 = unpool(pool1, conv1.get_shape(),64,64,2, S=2, scope="unpool1")
-        # unpool1 = max_indices_unpool(deconv2, pool1_indices, S=2, scope = "unpool1")
-        deconv1 = conv(unpool1, D=64, K=64, F=3, S=1, scope = "deconv1")
+    unpool1 = unpool(deconv2, conv1.get_shape(),64,64,2, S=2, scope="unpool1")
+    # unpool1 = max_indices_unpool(deconv2, pool1_indices, S=2, scope = "unpool1")
+    deconv1 = conv(unpool1, training,D=64, K=64, F=3, S=1, scope = "deconv1")
 
-    preds = tf.nn.softmax(conv(deconv1, D=64, K=12, F=1, S=1, scope="classify"))
+    logits = conv(deconv1, training, D=64, K=12, F=3, S=1, scope="final_logits")
     # logits = tf.cast(tf.argmax(preds, axis=3), tf.int32)
-    return  preds
+    return  logits
